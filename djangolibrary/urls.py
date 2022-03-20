859abdc24079ff1a1822e966d67bd30004b28a8a
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-20 10:45:49 -0300

    feat(books): image upload funcionalite

--- djangolibrary/urls.py
+++ djangolibrary/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 from rest_framework import routers
 from books.api import viewsets as books_viewsets
 
@@ -23,5 +25,5 @@ route.register('books', books_viewsets.BookViewSet, basename="Books")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-     path('', include(route.urls)),
-]
+    path('', include(route.urls)),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
