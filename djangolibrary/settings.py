b1ecfbb22e3708c1108700e5866a20762bd27176
Lucas Souza <lucasp.sdev@gmail.com>
2022-03-20 11:01:39 -0300

    chore: configure heroku inside project

--- djangolibrary/settings.py
+++ djangolibrary/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-@1rj39%ruxn6)aw-fj-$kf7k9=++kw3j#ma+(+znu736$7gotw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://django-library-api-erebor.herokuapp.com/']
 
 
 # Application definition
@@ -120,6 +121,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+django_heroku.settings(locals())
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
